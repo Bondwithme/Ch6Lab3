825473b1711952dce3d95ffa4421683f72089b5d
Carl Piercy <cpiercy111@gmail.com>
2018-01-05 09:50:27 -0800

    1/5

---
+++ Example.java
@@ -0,0 +1,12 @@
+
+public class Example
+{
+   public static void main(String[] args) {
+       ArrayList arr = new ArrayList();
+       ArrayList<String> arr2 = new
+       
+       arr.add(1);
+       arr.add("Hi");
+       System.out.println(arr);
+    }
+}
